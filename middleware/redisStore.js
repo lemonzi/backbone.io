5a2fbeee21153933d219b3b5e83a3c9797d74565
Quim Llimona <lemonzi42@gmail.com>
2013-08-02 18:50:02 -0700

    Avoid creating new functions with every request

--- middleware/redisStore.js
+++ middleware/redisStore.js
@@ -3,43 +3,46 @@ module.exports = function() {
     var models = {};
     var id = 1;
 
+    var crud = {
+
+        create: function(req, res, next) {
+            var model = req.model;
+            model.id = id++;
+            models[model.id] = model;
+            res.end(model);
+        },
+
+        read: function(req, res, next) {
+            if (req.model.id) {
+                res.end(models[req.model.id]);
+            } else {
+                var values = [];
+                for (var id in models) {
+                    values.push(models[id]);
+                }
+                res.end(values);
+            }
+        },
+
+        update: function(req, res, next) {
+            models[req.model.id] = req.model;
+            res.end(req.model);
+        },
+
+        delete: function(req, res, next) {
+            delete models[req.model.id];
+            res.end(req.model);
+        }
+
+    };
+
     return function(req, res, next) {
 
-        var crud = {
-            create: function() {
-                var model = req.model;
-                model.id = id++;
-                models[model.id] = model;
-                res.end(model);
-            },
-
-            read: function() {
-                if (req.model.id) {
-                    res.end(models[req.model.id]);
-                } else {
-                    var values = [];
-                    for (var id in models) {
-                        values.push(models[id]);
-                    }
-                    res.end(values);
-                }
-            },
-
-            update: function() {
-                models[req.model.id] = req.model;
-                res.end(req.model);
-            },
-
-            delete: function() {
-                delete models[req.model.id];
-                res.end(req.model);
-            }
-        };
-
-        if (!crud[req.method])
+        if (!crud[req.method]) {
             return next(new Error('Unsuppored method ' + req.method));
+        }
 
-        crud[req.method]();
+        crud[req.method](arguments);
 
     };
 
