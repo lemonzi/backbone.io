ad626923bc3574507c7b8882e471d98fdb46b00b
Quim Llimona <lemonzi42@gmail.com>
2013-08-12 20:52:51 -0700

    Backbone refactor

--- middleware/UNUSED_channel.js
+++ middleware/UNUSED_channel.js
@@ -15,3 +15,29 @@ module.exports = function(options) {
     };
 
 };
+
+
+
+
+
+
+socket.on('join', function(channel) {
+    socket.join(channel);
+});
+socket.on('leave', function(channel) {
+    socket.leave(channel);
+});
+socket.on('sync', function(req, callback) {
+    sync.handle(backends[backend], req, function(err, result) {
+        callback(err, result);
+        if (err) return;
+        if (req.method !== 'read') {
+            if (req.channel) {
+                req.socket.broadcast.to(req.channel || req.model.id)
+                          .emit(req.method, result);
+            } else if (options.broadcast) {
+                req.socket.broadcast.emit(req.method, result);
+            }
+        }
+    });
+});
