fae67fa56af0523cdd02d68f894d96c4b4d4ea0d
Quim Llimona <lemonzi42@gmail.com>
2013-08-13 19:21:14 -0700

    sockets and backbone now working

---
+++ middleware/channel.js
@@ -0,0 +1,43 @@
+module.exports = function(options) {
+
+    options = options || {};
+
+    var broadcast = function(req, res) {
+        var method = req.method;
+        var message = {
+            id: req.id,
+            channel: req.channel,
+            backend: req.backend,
+            entity: req.entity,
+            model: req.model
+        };
+        if (req.channel) {
+            req.socket.broadcast.to(req.channel).emit('msg', method, message);
+        } else if (req.broadcast || options.broadcast) {
+            req.socket.broadcast.emit('msg', method, message);
+        } else {
+            var channel = req.entity + ':' + req.id;
+            req.socket.broadcast.to(channel).emit('msg', method, message);
+        }
+    };
+
+    var apiHandlers = {
+        join: function(req, res) {
+            var channel = req.channel || (req.entity+':'+req.id);
+            req.socket.join(channel);
+        },
+        leave: function(req, res) {
+            var channel = req.channel || (req.entity+':'+req.id);
+            req.socket.leave(channel);
+        },
+        create: broadcast,
+        update: broadcast,
+        destroy: broadcast
+    };
+
+    return function(req, res, next) {
+        if (apiHandlers[req.method]) apiHandlers[req.method](req,res);
+        next();
+    };
+
+};
